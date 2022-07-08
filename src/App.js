46365dbce3d83848b6632a525873f8f7b7f3b454
Lucia <lucividal09@gmail.com>
2022-07-08 11:09:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { toHaveFormValues } from '@testing-library/jest-dom/dist/matchers'
+import { useState } from 'react'
 
-function App() {
+const Button = (prop) => <button onClick={prop.clickHandler}>{prop.text}</button>
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVotes] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0});
+  const handleVotes = () => {
+    const newVotes = {...vote}
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+  const maxvotes = () => {
+    let arr = Object.values(vote)
+    let index = arr.indexOf(Math.max(...arr))
+    return index
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button clickHandler={handleVotes} text="vote" />
+      <Button clickHandler = {() => setSelected(selected+1)} text="next anecdote"/>
+      <p>{anecdotes[selected]}</p>
+      <p>{vote[selected]} votes</p>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[maxvotes()]}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
